a5be6a61f76ae287dd02fb2ac9837450477f2a1a
vitalijs.komasilovs@gmail.com <vitalijs.komasilovs@gmail.com@5719385f-963f-8ea9-c3dc-985c5ef57f9a>
2013-03-14 17:43:57 +0000

    RKV 3nd task

---
+++ trunk/robot-sim/rkv/rkv_3/src/rkv_3/Action.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package rkv_3;
+
+/**
+ *
+ * @author vitaljok
+ */
+public enum Action {
+    LeftForward,
+    StraightForward,
+    RightForward,
+    LeftRverse,
+    StraightReverse,
+    RightReverse
+}
